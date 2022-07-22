fc26890e4d1e10c97cd59d352de19e027de3138e
Jamie Gabbay <90477271+jgbby@users.noreply.github.com>
2022-07-22 09:22:49 -0400

    Added Arm, DuckSweeper, and Grabber for campers, weird github issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Dropper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Dropper.java
@@ -2,9 +2,24 @@ package org.firstinspires.ftc.teamcode.Hardware;
 
 public class Dropper {
 
-    /*
+    /** TODO: ATTRIBUTES
+     *  - String left_id
+     *  - String right_id
+     *  - Servo left_servo
+     *  - Servo right_servo
+     *
+     *  - double LEFT_OPEN_POSITION = ...
+     *  - double LEFT_CLOSE_POSITION = ...
+     *  - double RIGHT_OPEN_POSITION = ...
+     *  - double RIGHT_CLOSE_POSITION = ...
+      */
 
-        Y O U R    C O D E    H E R E
-
+    /** TODO: CONSTRUCTOR
+     *  - Instantiate servos using the ids
      */
+
+    /**
+     *
+     */
+
 }
